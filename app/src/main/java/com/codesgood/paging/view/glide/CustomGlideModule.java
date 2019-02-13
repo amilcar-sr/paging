689d190d710dd71aec03eccfdad0ceda761d7c52
Amilcar Serrano <amilcar.sr8@gmail.com>
2019-02-13 16:01:03 -0600

    Added glide module to get rid of processor warning

---
+++ app/src/main/java/com/codesgood/paging/view/glide/CustomGlideModule.java
@@ -0,0 +1,8 @@
+package com.codesgood.paging.view.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class CustomGlideModule extends AppGlideModule {
+}
